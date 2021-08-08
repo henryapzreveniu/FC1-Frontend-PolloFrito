8cadf92d5a23cbfc9858cd87c3f7d8a2500357b9
Henry Aurelio Aymara Apaza <henrys4hgb@gmail.com>
2021-08-07 23:06:53 -0500

    second commit

--- pollo-frito/src/environments/environment.prod.ts
+++ pollo-frito/src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  REVENIU_URL: 'https://integration.reveniu.com/api/v1/',
+  API : 'http://localhost:3050/',
+  PLAN_ID: 475
 };

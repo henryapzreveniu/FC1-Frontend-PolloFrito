f54d2cab4d89a53321c84a3bf45e4a3397313487
Henry Aurelio Aymara Apaza <henrys4hgb@gmail.com>
2021-08-08 20:28:25 -0500

    changed api enviroment prod

--- pollo-frito/src/environments/environment.prod.ts
+++ pollo-frito/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   REVENIU_URL: 'https://integration.reveniu.com/api/v1/',
-  API : 'http://176.31.226.236:3050/',
+  API : 'https://friday-challenge1.herokuapp.com/',
   PLAN_ID: 475
 };
